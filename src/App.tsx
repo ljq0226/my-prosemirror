72733b47c40c23d8213cddf8deb44c35a78b3260
ljq0226 <1035821043@qq.com>
2023-08-09 08:04:26 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen p-[200px] bg-slate-300">
+      <h1 className="text-center text-2xl">My-ProseMirror-Editor</h1>
+      <div className="h-[800px] bg-white w-full">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
